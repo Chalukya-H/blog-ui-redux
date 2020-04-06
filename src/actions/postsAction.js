9aac651905fb6f51a8e796f0de535e4d6c3fb424
Chalukya H <62981809+Chalukya-H@users.noreply.github.com>
2020-04-06 12:13:01 +0530

    Added postsAction js file

---
+++ src/actions/postsAction.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+  
+export const setPosts = (posts) => {
+    return { type: 'SET_POSTS', payload: posts}
+}
+
+ 
+export const startGetUsers = () => {
+    return (dispatch) => {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then((response) =>{
+            const posts = response.data
+            dispatch(setPosts(posts))
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+    }
+}
