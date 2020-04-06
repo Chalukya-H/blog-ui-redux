d77eeb4091a7f14b9a6fcec5f46f9c30233dbffe
Chalukya H <62981809+Chalukya-H@users.noreply.github.com>
2020-04-06 10:48:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter,Link,Route} from 'react-router-dom'
 import './App.css';
+import Home from './home'
+import Users from './users'
+import UserShow from './userShow'
+import Posts from './posts'
+import PostShow from './postShow'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <BrowserRouter>
+     <div style = {{backgroundColor:'black' }}>
+         <Link to ='/'  style ={{fontSize:'30px',color:'white'}} > <button> Home</button></Link> 
+         <Link to='/users'  style ={{fontSize:'30px',color:'white'}} ><button> Users</button></Link> 
+         <Link to='/posts'  style ={{fontSize:'30px',color:'white'}} ><button> Posts</button> </Link> 
+
+
+         <Route path = "/" component = {Home} exact = {true}/>
+         <Route path = "/users" component = {Users} exact = {true}/>
+         <Route path = "/posts" component = {Posts} exact = {true}/>
+         <Route path = "/users/:id" component = {UserShow} exact = {true}/>
+         <Route path = "/posts/:id" component = {PostShow} exact = {true}/>
+      </div>
+     </BrowserRouter>
   );
 }
 
